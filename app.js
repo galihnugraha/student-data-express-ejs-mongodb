4c4d4f6a519b6beb13a0577d86c2a1b694b438af
Galih Nugraha <galihnugraha58@gmail.com>
2024-01-18 15:09:30 +0700

    express-ejs-layout

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
 const express = require('express')
+const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const port = 3000
 
-//ejs for view engine
+//ejs for view engine and layouts
 app.set('view engine', 'ejs')
+app.use(expressLayouts)
 
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        title: "Halaman Home",
+        layout: 'layouts/main-layout' 
+    })
 })
 
 app.get('/about', (req, res) => {
-    res.render('about')
+    res.render('about', {
+        title: "Halaman About",
+        layout: 'layouts/main-layout' 
+    })
 })
 
 app.get('/data', (req, res) => {
-    res.render('data')
+    res.render('data', {
+        title: "Data Mahasiswa",
+        layout: 'layouts/main-layout' 
+    })
 })
 
 app.use('/', (req, res) => {
